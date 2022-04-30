edf9ec7bef0b78a9b717f641fa03163652c61cca
nickid <92078139+khw2ok@users.noreply.github.com>
2022-05-01 04:00:07 +0900

    Update comments.js

--- javascript/comments.js
+++ javascript/comments.js
@@ -1,13 +1,74 @@
 var prevScrollpos = window.pageYOffset;
-window.onscroll =headerbarToggle
+window.onscroll = headerbarToggle
 
 function headerbarToggle() {
-    var headerbar = document.getElementById("navbar");
-    
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos < currentScrollPos - 200) {
-        headerbar.style.opacity = 0.2;  
-    } else {        
-        headerbar.style.opacity = 1;   
+    var btnbar = document.getElementById("btnbar")
+    var ftrbar = document.getElementById("ftrbar")
+
+    var currentScrollPos = window.pageYOffset
+    if (prevScrollpos < currentScrollPos - 400) {
+        btnbar.style.opacity = 0.1
     }
+    else if (prevScrollpos < currentScrollPos - 375) {
+        btnbar.style.opacity = 0.2
+    }
+    else if (prevScrollpos < currentScrollPos - 350) {
+        btnbar.style.opacity = 0.3
+    }
+    else if (prevScrollpos < currentScrollPos - 325) {
+        btnbar.style.opacity = 0.4
+    }
+    else if (prevScrollpos < currentScrollPos - 300) {
+        btnbar.style.opacity = 0.5
+    }
+    else if (prevScrollpos < currentScrollPos - 275) {
+        btnbar.style.opacity = 0.6
+    }
+    else if (prevScrollpos < currentScrollPos - 250) {
+        btnbar.style.opacity = 0.7
+    }
+    else if (prevScrollpos < currentScrollPos - 225) {
+        btnbar.style.opacity = 0.8
+    }
+    else if (prevScrollpos < currentScrollPos - 200) {
+        btnbar.style.opacity = 0.9
+    }
+    else {
+        btnbar.style.opacity = 1
+    }
+
+    if (prevScrollpos < currentScrollPos - 225) {
+        ftrbar.style.opacity = 0.1
+    }
+    else if (prevScrollpos < currentScrollPos - 200) {
+        ftrbar.style.opacity = 0.2
+    }
+    else if (prevScrollpos < currentScrollPos - 175) {
+        ftrbar.style.opacity = 0.3
+    }
+    else if (prevScrollpos < currentScrollPos - 150) {
+        ftrbar.style.opacity = 0.4
+    }
+    else if (prevScrollpos < currentScrollPos - 125) {
+        ftrbar.style.opacity = 0.5
+    }
+    else if (prevScrollpos < currentScrollPos - 100) {
+        ftrbar.style.opacity = 0.6
+    }
+    else if (prevScrollpos < currentScrollPos - 75) {
+        ftrbar.style.opacity = 0.7
+    }
+    else if (prevScrollpos < currentScrollPos - 50) {
+        ftrbar.style.opacity = 0.8
+    }
+    else if (prevScrollpos < currentScrollPos - 25) {
+        ftrbar.style.opacity = 0.9
+    }
+    else {
+        ftrbar.style.opacity = 1
+    }
+}
+
+function goUp() {
+    windows.scrollTo(0,0)
 }
